bb7dc344344e38ef436584614ff916eb954aae25
Wieger Wesselink <j.w.wesselink@tue.nl>
2022-12-06 10:26:21 +0100

    Rename a parameter

--- python/cifar10.py
+++ python/cifar10.py
@@ -184,7 +184,7 @@ def train_dense_model_with_augmentation(x_train, x_test, y_train, y_test):
     print('')
 
 
-def minibatch_gradient_descent_with_regrow(M, dataset, loss, learning_rate, epochs, batch_size, shuffle=True, statistics=True, zeta=0.3, weight_initializer=Weights.Xavier, rng=RandomNumberGenerator(1234567)):
+def minibatch_gradient_descent_with_regrow(M, dataset, loss, learning_rate, epochs, batch_size, shuffle=True, statistics=True, zeta=0.3, regrow_weights=Weights.Zero, rng=RandomNumberGenerator(1234567)):
     N = dataset.Xtrain.shape[1]  # the number of examples
     I = list(range(N))
     K = N // batch_size  # the number of batches
@@ -194,7 +194,7 @@ def minibatch_gradient_descent_with_regrow(M, dataset, loss, learning_rate, epoc
 
     for epoch in range(epochs):
         if epoch > 0:
-            M.regrow(zeta, weight_initializer, rng)
+            M.regrow(zeta, regrow_weights, rng)
 
         watch.reset()
         if shuffle:
@@ -228,7 +228,7 @@ def train_sparse_model_with_regrow(dataset):
     sparsity = 0.5
     model = create_sparse_model(sparsity)
     M = model.compile(input_size, batch_size, rng)
-    minibatch_gradient_descent_with_regrow(M, dataset, loss, learning_rate_scheduler, epochs=10, batch_size=100, shuffle=True, statistics=True, zeta=0.3, weight_initializer=Weights.Zero, rng=rng)
+    minibatch_gradient_descent_with_regrow(M, dataset, loss, learning_rate_scheduler, epochs=10, batch_size=100, shuffle=True, statistics=True, zeta=0.3, regrow_weights=Weights.Zero, rng=rng)
     print('')
 
 
@@ -239,8 +239,8 @@ if __name__ == '__main__':
     x_test_flattened = flatten(x_test)
     dataset = make_dataset(x_train_flattened, y_train, x_test_flattened, y_test)
 
-    # train_dense_model(dataset)
-    # train_dense_model_cpp(dataset)
-    # train_sparse_model(dataset)
+    train_dense_model(dataset)
+    train_dense_model_cpp(dataset)
+    train_sparse_model(dataset)
     train_sparse_model_with_regrow(dataset)
-    # train_dense_model_with_augmentation(x_train, x_test, y_train, y_test)
+    train_dense_model_with_augmentation(x_train, x_test, y_train, y_test)
