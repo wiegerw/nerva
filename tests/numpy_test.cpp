38dcc04e04b8179b3c6b25138dd7a991e8ec08c0
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-01-12 13:45:00 +0100

    Generalized Numpy array conversion functions

--- tests/numpy_test.cpp
+++ tests/numpy_test.cpp
@@ -19,42 +19,52 @@
 
 namespace py = pybind11;
 
-py::array_t<double> to_numpy(const Eigen::MatrixXd& A)
+template <typename Scalar, int MatrixLayout>
+py::array_t<Scalar> to_numpy(const Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, MatrixLayout>& A)
 {
+  using matrix_t = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, MatrixLayout>;
+
   // Create an empty array result of the correct size
-  py::array_t<double> result({A.rows(), A.cols()});
+  py::array_t<Scalar> result({A.rows(), A.cols()});
 
   // Copy A to result
-  Eigen::Map<Eigen::MatrixXd>(result.mutable_data(), A.rows(), A.cols()) = A;
+  Eigen::Map<matrix_t>(result.mutable_data(), A.rows(), A.cols()) = A;
 
   return result;
 }
 
-Eigen::MatrixXd from_numpy(const py::array_t<double>& A)
+template <typename Scalar = double, int MatrixLayout = Eigen::ColMajor>
+Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, MatrixLayout> from_numpy(const py::array_t<double>& A)
 {
+  using matrix_t = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, MatrixLayout>;
+
   assert(A.ndim() == 2);
   auto shape = A.shape();
-  return Eigen::Map<Eigen::MatrixXd>(const_cast<double*>(A.data()), shape[0], shape[1]);
+  return Eigen::Map<matrix_t>(const_cast<Scalar*>(A.data()), shape[0], shape[1]);
 }
 
 // Save a Numpy array to a file in .npy format
-void save_numpy_array(const std::string& filename, const py::array_t<double>& A)
+template <typename Scalar>
+void save_numpy_array(const std::string& filename, const py::array_t<Scalar>& A)
 {
   py::module::import("numpy").attr("save")(filename, A);
 }
 
-py::array_t<double> load_numpy_array(const std::string& filename)
+template <typename Scalar = double>
+py::array_t<Scalar> load_numpy_array(const std::string& filename)
 {
   return py::module::import("numpy").attr("load")(filename);
 }
 
 // compare two dimensional Numpy arrays
-bool compare_numpy_arrays(const py::array_t<double>& x, const py::array_t<double>& y)
+template <typename Scalar>
+bool compare_numpy_arrays(const py::array_t<Scalar>& x, const py::array_t<Scalar>& y)
 {
   return std::equal(x.data(), x.data() + x.size(), y.data(), y.data() + y.size());
 }
 
-void print_numpy_array(const py::array_t<double>& x)
+template <typename Scalar>
+void print_numpy_array(const py::array_t<Scalar>& x)
 {
   auto n = x.size();
   const double* data = x.data();
@@ -78,7 +88,7 @@ TEST_CASE("test_to_numpy")
   py::array_t<double> B = to_numpy(A);
   print_numpy_array(B);
 
-  Eigen::MatrixXd D = from_numpy(B);
+  auto D = from_numpy(B);
   CHECK(A == D);
 
   save_numpy_array("B.npy", B);
