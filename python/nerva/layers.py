3db5a5413e10e53eed432137b61fefa0b18e586e
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-01-24 10:54:07 +0100

    Fix printing of the model

--- python/nerva/layers.py
+++ python/nerva/layers.py
@@ -271,8 +271,8 @@ class Sequential(object):
         self.compiled_model.optimize(eta)
 
     def __str__(self):
-        layers = '.\n'.join([str(layer) for layer in self.layers])
-        return f'Sequential(\n{layers}\n)'
+        layers = ',\n  '.join([str(layer) for layer in self.layers])
+        return f'Sequential(\n  {layers}\n)'
 
     def info(self):
         self.compiled_model.info('M')
