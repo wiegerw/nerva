b35b5a83fa652cd1f707fc40664bc32fa4df677c
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-01-30 18:38:57 +0100

    Updated the augmentation script
    - It now does 5 repetitions of 100 epochs

--- python/augment_data.sh
+++ python/augment_data.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python3 augment_data.py --epochs=5 --outputdir=cifar10
+python3 augment_data.py --epochs=100 --seed=1 --outputdir=cifar1
+python3 augment_data.py --epochs=100 --seed=2 --outputdir=cifar2
+python3 augment_data.py --epochs=100 --seed=3 --outputdir=cifar3
+python3 augment_data.py --epochs=100 --seed=4 --outputdir=cifar4
+python3 augment_data.py --epochs=100 --seed=5 --outputdir=cifar5
