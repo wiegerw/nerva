da71e099203ac583df4d58dc56416cb36bbfb670
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-01-31 15:23:02 +0100

    Added an option to check if augmented data is saved correctly

--- python/augment_data.py
+++ python/augment_data.py
@@ -10,8 +10,8 @@ import sys
 
 import numpy as np
 import torch
-from testing.datasets import create_cifar10_datasets, create_dataloaders
-from testing.numpy_utils import load_eigen_array, save_eigen_array, pp
+from testing.datasets import create_cifar10_datasets, create_dataloaders, load_cifar10_data
+from testing.numpy_utils import pp
 
 
 def load_models(model: str, datadir: str):
@@ -43,6 +43,33 @@ def inspect_data(outputdir, epochs):
         pp(f'Xtrain', Xtrain)
 
 
+# check if the data is stored correctly
+def check(datadir):
+    import tempfile
+    filename = tempfile.NamedTemporaryFile().name + '_cifar.npz'
+
+    Xtrain, Ttrain, Xtest, Ttest = load_cifar10_data(datadir)
+    pp('Xtrain', Xtrain)
+
+    # save the data to .npz
+    print(f'Saving data to file {filename}')
+    with open(filename, "wb") as f:
+        np.savez_compressed(f,
+                            Xtrain=to_eigen(Xtrain.detach().numpy()),
+                            Ttrain=to_eigen(Ttrain.detach().numpy()),
+                            Xtest=to_eigen(Xtest.detach().numpy()),
+                            Ttest=to_eigen(Ttest.detach().numpy())
+                            )
+
+    # load the .npz data
+    print(f'Loading data from file {filename}')
+    d = np.load(filename)
+    Xtrain_new = from_eigen(d['Xtrain'])
+    pp(f'Xtrain_new', Xtrain_new)
+
+    pathlib.Path(filename).unlink()
+
+
 def main():
     cmdline_parser = argparse.ArgumentParser()
     cmdline_parser.add_argument("--batch-size", help="The batch size", type=int, default=1)
@@ -52,10 +79,15 @@ def main():
     cmdline_parser.add_argument('--datadir', type=str, default='./data', help='the data directory (default: ./data)')
     cmdline_parser.add_argument("--outputdir", type=str, help="the output directory where the results are stored")
     cmdline_parser.add_argument("--inspect", help="if this flag is set, the output directory will be inspected", action="store_true")
+    cmdline_parser.add_argument("--check", help="check if the data is the same after saving and loading", action="store_true")
     args = cmdline_parser.parse_args()
 
     if args.seed:
         torch.manual_seed(args.seed)
+
+    if args.check:
+        check(args.datadir)
+        sys.exit(0)
 
     if args.inspect:
         inspect_data(args.outputdir, args.epochs)
