c221e2ce24f1a84560430a81a2ef463277ae5137
Bram Grooten <b.j.grooten@student.tue.nl>
2023-01-31 15:29:21 +0100

    x-vs-density plot (x can be acc or time)

--- python/snn/plot_x-vs-density.py
+++ python/snn/plot_x-vs-density.py
@@ -36,39 +36,31 @@ def parse_logfile(path: pathlib.Path) -> pd.DataFrame:
                          'test accuracy': test_accuracy,
                          'time': time})
 
-
-
-def plot_one_log(df: pd.DataFrame, path: pathlib.Path):
-    # Plot 'epoch' vs 'train accuracy'
-    plt.plot(df['epoch'], df['train accuracy'], label='train accuracy')
-
-    # Plot 'epoch' vs 'test accuracy'
-    plt.plot(df['epoch'], df['test accuracy'], label='test accuracy', color='red')
+def make_acc_vs_density_plot(df: pd.DataFrame, path: pathlib.Path, x_axis: str = 'density'):
+    # plot 'accuracy' vs 'density' for each framework using seaborn
+    sns.lineplot(x=x_axis, y='test accuracy', hue='framework', data=df, marker='o')
 
     # Add labels and title to the plot
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.title(f'Accuracy vs Epoch ({path})')
-
+    xlabel = x_axis[0].upper() + x_axis[1:]  # make first letter x_axis uppercase
+    plt.xlabel(xlabel)
+    plt.ylabel('Best test accuracy')
+    plt.title(f'Accuracy vs {xlabel}')
     # Add a legend to the plot
     plt.legend()
-
-    # Show the plot
-    # plt.show()
-
     # Save the plot
-    plt.savefig(path.with_suffix('.png'))
+    plt.savefig(path)
     plt.close()
 
 
-def make_acc_vs_density_plot(df: pd.DataFrame, path: pathlib.Path):
-    # plot 'accuracy' vs 'density' for each framework using seaborn
-    sns.lineplot(x='density', y='test accuracy', hue='framework', data=df)
+def make_time_vs_density_plot(df: pd.DataFrame, path: pathlib.Path, x_axis: str = 'density'):
+    # plot 'time' vs 'density/sparsity' for each framework using seaborn
+    sns.lineplot(x=x_axis, y='time', hue='framework', data=df, marker='o')
 
     # Add labels and title to the plot
-    plt.xlabel('Density')
-    plt.ylabel('Best test accuracy')
-    plt.title(f'Accuracy vs Density')
+    xlabel = x_axis[0].upper() + x_axis[1:]  # make first letter x_axis uppercase
+    plt.xlabel(xlabel)
+    plt.ylabel('Training time (minutes)')
+    plt.title(f'Time vs {xlabel}')
     # Add a legend to the plot
     plt.legend()
     # Save the plot
@@ -92,16 +84,23 @@ def main():
 
         df = pd.concat([df, data])
 
-        # change path to start with ./plots instead of ./logs
-        # path = pathlib.Path('./plots') / path.name
-        # plot_one_log(data, path)
-
+    # make a new df with the total time summed for each framework and density and seed
+    df_time = df.drop(columns=['epoch', 'lr', 'loss', 'train accuracy', 'test accuracy'])
+    df_time = df_time.groupby(['framework', 'density', 'seed']).sum().reset_index()
+    df_time['time'] = df_time['time'] / 60  # convert time to minutes
+    df_time['sparsity'] = 1 - df_time['density']
 
     # make a new df with the best test accuracy for each framework and density and seed
     df_best = df.drop(columns=['epoch', 'time', 'loss'])
     df_best = df_best.groupby(['framework', 'density', 'seed']).max().reset_index()
+    df_best['sparsity'] = 1 - df_best['density']
 
-    make_acc_vs_density_plot(df_best, pathlib.Path('./plots/accuracy-vs-density.png'))
+
+    # x_axis = 'sparsity'
+    x_axis = 'density'
+
+    # make_time_vs_density_plot(df_time, pathlib.Path(f'./plots/time-vs-{x_axis}.png'), x_axis)
+    make_acc_vs_density_plot(df_best, pathlib.Path(f'./plots/accuracy-vs-{x_axis}.png'), x_axis)
 
 
 
