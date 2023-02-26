a64115e4fc4eefd76e6a76d997d3a58e071edc76
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-02-26 22:00:35 +0100

    Added tests for the MKL dense matrix class

--- include/nerva/neural_networks/mkl_dense_matrix.h
+++ include/nerva/neural_networks/mkl_dense_matrix.h
@@ -16,56 +16,25 @@
 
 namespace nerva::mkl {
 
-enum class matrix_layout
+// Uses the same values as Eigen
+enum matrix_layout
 {
-  row_major,
-  column_major
+  column_major = 0,
+  row_major = 0x1
 };
 
 // This class can be used to wrap an Eigen matrix (or NumPy etc.)
-template <typename Scalar, matrix_layout layout>
-class dense_matrix_view;
-
-template <typename Scalar, matrix_layout layout>
-class dense_matrix_view<const Scalar, layout>
-{
-  protected:
-    long m; // number of rows
-    long n; // number of columns
-    const Scalar* values;
-
-  public:
-    dense_matrix_view(long m_, long n_, const Scalar* values_)
-      : m(m_), n(n_), values(values_)
-    {}
-
-    [[nodiscard]] long rows() const
-    {
-      return m;
-    }
-
-    [[nodiscard]] long cols() const
-    {
-      return n;
-    }
-
-    const Scalar* data() const
-    {
-      return values;
-    }
-};
-
-template <typename Scalar, matrix_layout layout>
+template <typename Scalar, int MatrixLayout>
 class dense_matrix_view
 {
   protected:
+    Scalar* values;
     long m; // number of rows
     long n; // number of columns
-    Scalar* values;
 
   public:
-    dense_matrix_view(long m_, long n_, Scalar* values_)
-      : m(m_), n(n_), values(values_)
+    dense_matrix_view(Scalar* values_, long m_, long n_)
+      : values(values_), m(m_), n(n_)
     {}
 
     [[nodiscard]] long rows() const
@@ -88,6 +57,85 @@ class dense_matrix_view
       return values;
     }
 };
+
+template <typename Scalar, int MatrixLayout>
+class dense_matrix
+{
+  protected:
+    std::vector<Scalar> values;
+    long m; // number of rows
+    long n; // number of columns
+
+  public:
+    dense_matrix(long m_, long n_)
+      : values(m_ * n_, 0), m(m_), n(n_)
+    {}
+
+    [[nodiscard]] long rows() const
+    {
+      return m;
+    }
+
+    [[nodiscard]] long cols() const
+    {
+      return n;
+    }
+
+    Scalar* data()
+    {
+      return values.data();
+    }
+
+    const Scalar* data() const
+    {
+      return values.data();
+    }
+};
+
+// Computes the matrix product C = A * B
+template <typename Scalar, int MatrixLayout, template <typename, int> class Matrix1, template <typename, int> class Matrix2>
+dense_matrix<Scalar, MatrixLayout> matrix_product(const Matrix1<Scalar, MatrixLayout>& A, const Matrix2<Scalar, MatrixLayout>& B)
+{
+  assert(A.cols() == B.rows());
+  dense_matrix<Scalar, MatrixLayout> C(A.rows(), B.cols());
+
+  CBLAS_TRANSPOSE transA = CblasNoTrans;
+  CBLAS_TRANSPOSE transB = CblasNoTrans;
+
+  if constexpr (MatrixLayout == column_major)
+  {
+    int lda = A.rows();
+    int ldb = B.rows();
+    int ldc = C.rows();
+    double alpha = 1.0;
+    double beta = 0.0;
+    if constexpr (std::is_same<Scalar, double>::value)
+    {
+      cblas_dgemm(CblasColMajor, transA, transB, C.rows(), C.cols(), A.cols(), alpha, A.data(), lda, B.data(), ldb, beta, C.data(), ldc);
+    }
+    else
+    {
+      cblas_sgemm(CblasColMajor, transA, transB, C.rows(), C.cols(), A.cols(), alpha, A.data(), lda, B.data(), ldb, beta, C.data(), ldc);
+    }
+  }
+  else
+  {
+    int lda = A.cols();
+    int ldb = B.cols();
+    int ldc = C.cols();
+    double alpha = 1.0;
+    double beta = 0.0;
+    if constexpr (std::is_same<Scalar, double>::value)
+    {
+      cblas_dgemm(CblasRowMajor, transA, transB, C.rows(), C.cols(), A.cols(), alpha, A.data(), lda, B.data(), ldb, beta, C.data(), ldc);
+    }
+    else
+    {
+      cblas_sgemm(CblasRowMajor, transA, transB, C.rows(), C.cols(), A.cols(), alpha, A.data(), lda, B.data(), ldb, beta, C.data(), ldc);
+    }
+  }
+  return C;
+}
 
 } // namespace nerva::mkl
 
---
+++ tests/mkl_matrix_test.cpp
@@ -0,0 +1,103 @@
+// Copyright: Wieger Wesselink 2022
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mkl_matrix_test.cpp
+/// \brief Tests for Intel MKL libraries.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+#include <Eigen/Dense>
+#include "nerva/neural_networks/mkl_matrix.h"
+#include <iostream>
+
+using namespace nerva;
+
+using vector = Eigen::VectorXd;
+using matrix = Eigen::MatrixXd;
+using matrix_wrapper = Eigen::Map<matrix>;
+
+inline
+long nonzero_count(const matrix& A)
+{
+  return (A.array() == 0.0).count();
+}
+
+template <typename Scalar, int MatrixLayout>
+mkl::dense_matrix_view<Scalar, MatrixLayout> make_view(const Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, MatrixLayout>& A)
+{
+  return mkl::dense_matrix_view<Scalar, MatrixLayout>(const_cast<Scalar*>(A.data()), A.rows(), A.cols());
+}
+
+template <typename Scalar, int MatrixLayout>
+Eigen::Map<Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, MatrixLayout>> to_eigen(mkl::dense_matrix<Scalar, MatrixLayout>& A)
+{
+  return { A.data(), A.rows(), A.cols() };
+}
+
+template <typename Scalar, int MatrixLayout>
+void test_dense_dense_multiplication(const Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, MatrixLayout>& A,
+                                     const Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, MatrixLayout>& B)
+{
+  Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, MatrixLayout> C = A * B;
+  auto A1 = make_view(A);
+  auto B1 = make_view(B);
+  auto C1 = mkl::matrix_product(A1, B1);
+
+  scalar epsilon = std::is_same<Scalar, double>::value ? 1e-10 : 1e-5;
+
+  std::cout << "--- test_dense_dense_multiplication ---" << std::endl;
+  CHECK_LE((C - to_eigen(C1)).squaredNorm(), epsilon);
+}
+
+void test_dense_dense_multiplication(long m, long k, long n)
+{
+  using matrix1 = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor>;
+  using matrix2 = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+  using matrix3 = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor>;
+  using matrix4 = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
+  matrix1 A1 = matrix1::Random(m, k);
+  matrix1 B1 = matrix1::Random(k, n);
+  test_dense_dense_multiplication(A1, B1);
+
+  matrix1 A2 = matrix2::Random(m, k);
+  matrix1 B2 = matrix2::Random(k, n);
+  test_dense_dense_multiplication(A2, B2);
+
+  matrix3 A3 = matrix3::Random(m, k);
+  matrix3 B3 = matrix3::Random(k, n);
+  test_dense_dense_multiplication(A3, B3);
+
+  matrix4 A4 = matrix4::Random(m, k);
+  matrix4 B4 = matrix4::Random(k, n);
+  test_dense_dense_multiplication(A4, B4);
+}
+
+TEST_CASE("test_mkl1")
+{
+  matrix A {
+    {2, 3},
+    {7, 4},
+    {1, 8},
+  };
+
+  matrix B {
+    {1, 2, 3},
+    {4, 5, 6}
+  };
+
+  test_dense_dense_multiplication(A, B);
+}
+
+TEST_CASE("test_mkl2")
+{
+  test_dense_dense_multiplication(2, 3, 4);
+  test_dense_dense_multiplication(7, 1, 10);
+  test_dense_dense_multiplication(12, 8, 5);
+  test_dense_dense_multiplication(1, 8, 5);
+  test_dense_dense_multiplication(1, 8, 1);
+}
