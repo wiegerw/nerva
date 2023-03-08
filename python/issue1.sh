15e81ee038a817b7d02fab16a4bf9e82b704044b
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-03-08 15:08:55 +0100

    Added some more tests to issue1.sh

--- python/issue1.sh
+++ python/issue1.sh
@@ -10,8 +10,20 @@ source utilities.sh
 #
 # It is unknown what causes the slowdown in epoch 2 and epoch 3
 
-print_header "Export weights from PyTorch model"
-python3 -u mlp.py --torch --seed=1 --overall-density=0.001 --lr=0.1 --sizes=3072,1024,1024,1024,10 --batch-size=100 --epochs=0 --momentum=0.9 --nesterov --preprocessed=./cifar1 --precision=8 --save-weights=weights-0.001.npz 2>&1 | tee log1
+print_header "PyTorch export weights"
+python3 -u mlp.py --torch --seed=1 --overall-density=0.001 --lr=0.1 --sizes=3072,1024,1024,1024,10 --batch-size=100 --epochs=3 --momentum=0.9 --nesterov --datadir=./data --precision=8 --save-weights=weights-pytorch-0.001.npz 2>&1 | tee log1
 
-print_header "Import weights to Nerva model"
-../tools/dist/mlpf --seed=1 --overall-density=0.001 --sizes=3072,1024,1024,1024,10 --batch-size=100 --epochs=3 '--learning-rate=constant(0.1)' '--optimizer=nesterov(0.9)' --architecture=RRRL --weights=XXXX --loss=softmax-cross-entropy --threads=4 --no-shuffle --verbose --dataset=cifar10 --load-weights=weights-0.001.npz 2>&1 | tee log2
+print_header "Nerva export weights"
+../tools/dist/mlpf --seed=1 --overall-density=0.001 --sizes=3072,1024,1024,1024,10 --batch-size=100 --epochs=3 '--learning-rate=constant(0.1)' '--optimizer=nesterov(0.9)' --architecture=RRRL --weights=XXXX --loss=softmax-cross-entropy --threads=4 --no-shuffle --verbose --dataset=cifar10 --save-weights=weights-nerva-0.001.npz 2>&1 | tee log2
+
+print_header "Nerva import Nerva weights"
+../tools/dist/mlpf --seed=1 --overall-density=0.001 --sizes=3072,1024,1024,1024,10 --batch-size=100 --epochs=3 '--learning-rate=constant(0.1)' '--optimizer=nesterov(0.9)' --architecture=RRRL --weights=XXXX --loss=softmax-cross-entropy --threads=4 --no-shuffle --verbose --dataset=cifar10 --load-weights=weights-nerva-0.001.npz 2>&1 | tee log4
+
+print_header "Nerva import PyTorch weights"
+../tools/dist/mlpf --seed=1 --overall-density=0.001 --sizes=3072,1024,1024,1024,10 --batch-size=100 --epochs=3 '--learning-rate=constant(0.1)' '--optimizer=nesterov(0.9)' --architecture=RRRL --weights=XXXX --loss=softmax-cross-entropy --threads=4 --no-shuffle --verbose --dataset=cifar10 --load-weights=weights-pytorch-0.001.npz 2>&1 | tee log3
+
+print_header "PyTorch import Nerva weights"
+python3 -u mlp.py --torch --seed=1 --overall-density=0.001 --lr=0.1 --sizes=3072,1024,1024,1024,10 --batch-size=100 --epochs=3 --momentum=0.9 --nesterov --datadir=./data --precision=8 --load-weights=weights-nerva-0.001.npz 2>&1 | tee log6
+
+print_header "PyTorch import PyTorch weights"
+python3 -u mlp.py --torch --seed=1 --overall-density=0.001 --lr=0.1 --sizes=3072,1024,1024,1024,10 --batch-size=100 --epochs=3 --momentum=0.9 --nesterov --datadir=./data --precision=8 --load-weights=weights-pytorch-0.001.npz 2>&1 | tee log5
