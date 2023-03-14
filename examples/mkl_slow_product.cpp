4f260775b225398f6910845a03cfaf20426d4c41
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-03-14 11:33:19 +0100

    Separate the measurements for the MKL and the manual matrix product

--- examples/mkl_slow_product.cpp
+++ examples/mkl_slow_product.cpp
@@ -11,6 +11,9 @@ void product()
   Eigen::MatrixXf A = Eigen::MatrixXf::Random(1024, 100);
   Eigen::MatrixXf B = Eigen::MatrixXf::Random(100, 1024);
 
+  std::vector<double> mkl_times;
+  std::vector<double> manual_times;
+
   // create a random permutation of 0, 1, ..., 1023
   std::vector<long> I(1024);
   std::iota(I.begin(), I.end(), 0);
@@ -21,25 +24,41 @@ void product()
   Eigen::MatrixXf C = A * B;
 
   nerva::utilities::stopwatch watch;
-  for (long i: I)
+  for (std::size_t i = 0; i < I.size(); i++)
   {
-    watch.reset();
-    Eigen::MatrixXf AB = A * B;
-    auto seconds = watch.seconds();
-    std::cout << "time mkl = " << seconds << std::endl;
+    if (i % 10 == 0)
+    {
+      std::cout << "i = " << i << std::endl;
+      watch.reset();
+      Eigen::MatrixXf AB = A * B;
+      auto seconds = watch.seconds();
+      mkl_times.push_back(seconds);
+      watch.reset();
 
-    watch.reset();
-    auto A1 = nerva::mkl::make_dense_matrix_view(A);
-    auto B1 = nerva::mkl::make_dense_matrix_view(B);
-    auto C1 = nerva::mkl::ddd_product_manual_loops<nerva::mkl::column_major>(A1, B1, false, false);
-    seconds = watch.seconds();
-    std::cout << "time manual = " << seconds << std::endl;
+      auto A1 = nerva::mkl::make_dense_matrix_view(A);
+      auto B1 = nerva::mkl::make_dense_matrix_view(B);
+      auto C1 = nerva::mkl::ddd_product_manual_loops<nerva::mkl::column_major>(A1, B1, false, false);
+      seconds = watch.seconds();
+      manual_times.push_back(seconds);
+    }
 
     // fill the i-th row with small values
     for (long j = 0; j < 100; j++)
     {
       A(i, j) = 1e-40;
     }
+  }
+
+  std::cout << "--- mkl product times ---" << std::endl;
+  for (double t: mkl_times)
+  {
+    std::cout << t << std::endl;
+  }
+
+  std::cout << "--- manual product times ---" << std::endl;
+  for (double t: manual_times)
+  {
+    std::cout << t << std::endl;
   }
 }
 
