0c9749db2a68551d5911d3fcb0f298229779a629
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-04-02 12:42:25 +0200

    Put function for argument parsing in a separate file

---
+++ include/nerva/utilities/parse.h
@@ -0,0 +1,37 @@
+// Copyright: Wieger Wesselink 2023
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file nerva/utilities/parse.h
+/// \brief add your file description here.
+
+#ifndef NERVA_UTILITIES_PARSE_H
+#define NERVA_UTILITIES_PARSE_H
+
+#include "nerva/utilities/string_utility.h"
+
+namespace nerva::utilities {
+
+inline
+std::vector<std::string> parse_arguments(const std::string& text, unsigned int expected_size)
+{
+  auto pos1 = text.find_first_of('(');
+  auto pos2 = text.find_last_of(')');
+  if (pos1 == std::string::npos || pos2 == std::string::npos)
+  {
+    throw std::runtime_error("could not parse arguments of string '" + text + "'");
+  }
+  std::string arguments_text = text.substr(pos1 + 1, pos2 - pos1 - 1);
+  auto result = utilities::regex_split(arguments_text, ",");
+  if (result.size() != expected_size)
+  {
+    throw std::runtime_error("the string '" + text + "' has an unexpected number of arguments");
+  }
+  return result;
+}
+
+} // namespace nerva::utilities
+
+#endif // NERVA_UTILITIES_PARSE_H
