13c30d1e330fbaf541bf92da4eb7d8a4af05a809
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-04-24 14:14:21 +0200

    Added a script for a timing experiment

---
+++ python/mlp-timer.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+source utilities.sh
+
+seed=3
+init_weights=XavierNormalized
+density=0.05
+sizes="3072,1024,512,10"
+layers="ReLU;ReLU;Linear"
+optimizers="Nesterov(0.9)"
+learning_rate="Constant(0.1)"
+loss=SoftmaxCrossEntropy
+batch_size=100
+epochs=5
+
+print_header "Train CIFAR10 using mlp.py"
+python3 -u mlp.py \
+	--seed=$seed \
+	--overall-density=$density \
+	--batch-size=$batch_size \
+	--epochs=$epochs \
+	--sizes=$sizes \
+	--layers=$layers \
+	--optimizers=$optimizers \
+	--init-weights=$init_weights \
+	--learning-rate=$learning_rate \
+	--loss=$loss \
+	--preprocessed=./cifar$seed \
+	--timer \
+	2>&1 | tee mlptimer1.log
+
+print_header "Train CIFAR10 using mlp.cpp"
+../tools/dist/mlp \
+	--seed=$seed \
+	--overall-density=$density \
+	--batch-size=$batch_size \
+	--epochs=$epochs \
+	--sizes=$sizes \
+	--layers=$layers \
+	--optimizers=$optimizers \
+	--init-weights=$init_weights \
+	--learning-rate=$learning_rate \
+	--loss=$loss \
+	--preprocessed=./cifar$seed \
+	--threads=4 \
+	--no-shuffle \
+	--verbose \
+	--timer \
+	2>&1 | tee mlptimer2.log
