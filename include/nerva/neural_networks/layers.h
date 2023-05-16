9b7bce359818ade4a350f7787f2ae330ade4505f
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-05-16 11:16:17 +0200

    Refactored some code

--- include/nerva/neural_networks/layers.h
+++ include/nerva/neural_networks/layers.h
@@ -1,4 +1,4 @@
-// Copyright: Wieger Wesselink 2022
+// Copyright: Wieger Wesselink 2022 - present
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
@@ -11,6 +11,7 @@
 #define NERVA_NEURAL_NETWORKS_LAYERS_H
 
 #include "nerva/neural_networks/activation_functions.h"
+#include "nerva/neural_networks/layer_algorithms.h"
 #include "nerva/neural_networks/mkl_eigen.h"
 #include "nerva/neural_networks/mkl_sparse_matrix.h"
 #include "nerva/neural_networks/optimizers.h"
@@ -28,28 +29,6 @@ namespace nerva {
 template <typename Matrix> struct linear_layer;
 template <typename Matrix> struct dropout_layer;
 template <typename Matrix> struct batch_normalization_layer;
-
-inline
-void compare_sizes(const eigen::matrix& W1, const eigen::matrix& W2)
-{
-  if (W1.rows() != W2.rows() || W1.cols() != W2.cols())
-  {
-    eigen::print_numpy_matrix("W", W1);
-    eigen::print_numpy_matrix("W", W2);
-    throw std::runtime_error("matrix sizes do not match");
-  }
-}
-
-inline
-void compare_sizes(const mkl::sparse_matrix_csr<scalar>& W1, const eigen::matrix& W2)
-{
-  if (W1.rows() != W2.rows() || W1.cols() != W2.cols())
-  {
-    eigen::print_numpy_matrix("W", mkl::to_eigen(W1));
-    eigen::print_numpy_matrix("W", W2);
-    throw std::runtime_error("matrix sizes do not match");
-  }
-}
 
 struct neural_network_layer
 {
@@ -614,87 +593,6 @@ void set_optimizer(linear_layer<Matrix>& layer, const std::string& text)
   {
     throw std::runtime_error("unknown optimizer '" + text + "'");
   }
-}
-
-inline
-std::vector<double> compute_sparse_layer_densities(double overall_density,
-                                                   const std::vector<std::size_t>& layer_sizes,
-                                                   double erk_power_scale = 1
-                                                  )
-{
-  std::vector<std::pair<std::size_t, std::size_t>> layer_shapes;
-  for (std::size_t i = 0; i < layer_sizes.size() - 1; i++)
-  {
-    layer_shapes.emplace_back(layer_sizes[i], layer_sizes[i+1]);
-  }
-
-  std::size_t n = layer_shapes.size(); // the number of layers
-
-  if (overall_density == 1)
-  {
-    return std::vector<double>(n, 1);
-  }
-
-  std::set<std::size_t> dense_layers;
-  std::vector<double> raw_probabilities(n, double(0));
-  double epsilon;
-
-  while (true)
-  {
-    double divisor = 0;
-    double rhs = 0;
-    std::fill(raw_probabilities.begin(), raw_probabilities.end(), double(0));
-    for (std::size_t i = 0; i < n; i++)
-    {
-      auto [rows, columns] = layer_shapes[i];
-      auto N = rows * columns;
-      auto num_ones = N * overall_density;
-      auto num_zeros = N - num_ones;
-      if (dense_layers.count(i))
-      {
-        rhs -= num_zeros;
-      }
-      else
-      {
-        rhs += num_ones;
-        raw_probabilities[i] = ((rows + columns) / (double)(rows * columns)) * std::pow(erk_power_scale, double(1));
-        divisor += raw_probabilities[i] * N;
-      }
-    }
-    epsilon = rhs / divisor;
-    double max_prob = *std::max_element(raw_probabilities.begin(), raw_probabilities.end());
-    double max_prob_one = max_prob * epsilon;
-    if (max_prob_one > 1)
-    {
-      for (std::size_t j = 0; j < n; j++)
-      {
-        if (raw_probabilities[j] == max_prob)
-        {
-          dense_layers.insert(j);
-        }
-      }
-    }
-    else
-    {
-      break;
-    }
-  }
-
-  // Compute the densities
-  std::vector<double> densities(n, 0);
-  for (std::size_t i = 0; i < n; i++)
-  {
-    if (dense_layers.count(i))
-    {
-      densities[i] = 1;
-    }
-    else
-    {
-      double probability_one = epsilon * raw_probabilities[i];
-      densities[i] = probability_one;
-    }
-  }
-  return densities;
 }
 
 } // namespace nerva
