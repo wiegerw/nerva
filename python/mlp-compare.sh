ffc3e88fea2ddc9aca1c7466077ad4f6fac18899
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-06-19 08:52:52 +0200

    Added a script to compare a Nerva c++ and a Nerva python run

---
+++ python/mlp-compare.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This script compares the computations of Nerva C++ and Nerva python by
+# logging intermediate results.
+
+source utilities.sh
+
+seed=1
+init_weights=XavierNormalized
+density=1.0
+sizes="3072,1024,512,10"
+layers="ReLU;ReLU;Linear"
+optimizers="Nesterov(0.9)"
+learning_rate="Constant(0.1)"
+loss=SoftmaxCrossEntropy
+batch_size=100
+epochs=1
+
+print_header "Train CIFAR10 using mlp.cpp"
+../tools/dist/mlp \
+  --seed=$seed \
+  --overall-density=$density \
+  --batch-size=$batch_size \
+  --epochs=$epochs \
+  --sizes=$sizes \
+  --layers=$layers \
+  --optimizers=$optimizers \
+  --init-weights=$init_weights \
+  --save-weights="mlp-compare.npz" \
+  --learning-rate=$learning_rate \
+  --loss=$loss \
+  --preprocessed=cifar$seed \
+  --threads=4 \
+  --no-shuffle \
+  --debug \
+  2>&1 | tee mlp-compare-cpp.log
+
+print_header "Train CIFAR10 using mlp.py"
+python3 -u mlp.py \
+  --seed=$seed \
+  --overall-density=$density \
+  --batch-size=$batch_size \
+  --epochs=$epochs \
+  --sizes=$sizes \
+  --layers=$layers \
+  --optimizers=$optimizers \
+  --load-weights="mlp-compare.npz" \
+  --learning-rate=$learning_rate \
+  --loss=$loss \
+  --preprocessed=cifar$seed \
+  --debug \
+  2>&1 | tee mlp-compare-python.log
+
+meld mlp-compare-cpp.log mlp-compare-python.log
