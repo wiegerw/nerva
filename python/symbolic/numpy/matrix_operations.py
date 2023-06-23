Applied some fixes for batch normalization
- diag(x).T should be replaced by diag(x) in case of 1-dim. vectors

python/symbolic/numpy/matrix_operations.py
@@ -34,11 +34,11 @@ def zeros(m: int, n=None) -> Matrix:
     return np.zeros((m, n)) if n else np.zeros(m)
 
 
-def ones(m: int, n: int = 1) -> Matrix:
+def ones(m: int, n=None) -> Matrix:
     """
     Returns an mxn matrix with all elements equal to 1.
     """
-    return np.ones((m, n))
+    return np.ones((m, n)) if n else np.ones(m)
 
 
 def identity(n: int) -> Matrix:

python/symbolic/tensorflow/layers_rowwise.py
@@ -292,7 +292,7 @@ class BatchNormalizationLayer(Layer):
         beta = self.beta
 
         R = X - row_repeat(columns_mean(X), N)
-        Sigma = tf.transpose(diag(tf.transpose(R) @ R)) / N
+        Sigma = diag(tf.transpose(R) @ R) / N
         power_minus_half_Sigma = power_minus_half(Sigma)
         Z = hadamard(row_repeat(power_minus_half_Sigma, N), R)
         Y = hadamard(row_repeat(gamma, N), Z) + row_repeat(beta, N)
@@ -310,7 +310,7 @@ class BatchNormalizationLayer(Layer):
         DZ = hadamard(row_repeat(gamma, N), DY)
         Dbeta = columns_sum(DY)
         Dgamma = columns_sum(hadamard(Z, DY))
-        DX = hadamard(row_repeat(power_minus_half_Sigma / N, N), (N * identity(N) - ones(N, N)) @ DZ - hadamard(Z, row_repeat(tf.transpose(diag(tf.transpose(Z) @ DZ)), N)))
+        DX = hadamard(row_repeat(power_minus_half_Sigma / N, N), (N * identity(N) - ones(N, N)) @ DZ - hadamard(Z, row_repeat(diag(tf.transpose(Z) @ DZ), N)))
 
         self.DZ.assign(DZ)
         self.Dbeta.assign(Dbeta)