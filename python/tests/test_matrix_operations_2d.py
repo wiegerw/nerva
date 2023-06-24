5c2267378c04fd1dc153edf6a55e2b08139f2945
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-06-24 11:44:32 +0200

    Generalized the check_equal functions

--- python/tests/test_matrix_operations_2d.py
+++ python/tests/test_matrix_operations_2d.py
@@ -17,29 +17,22 @@ from symbolic.utilities import to_numpy, to_sympy, to_torch, to_tensorflow
 
 class TestMatrixOperations(TestCase):
 
-    def check_arrays_equal(self, operation, x1, x2, x3, x4):
+    def check_arrays_equal(self, operation, values):
         print(f'--- {operation} ---')
-        x1 = to_numpy(x1)
-        x2 = to_numpy(x2)
-        x3 = to_numpy(x3)
-        x4 = to_numpy(x4)
-        print(x1)
-        print(x2)
-        print(x3)
-        print(x4)
-        self.assertTrue(np.allclose(x1, x2))
-        self.assertTrue(np.allclose(x1, x3))
-        self.assertTrue(np.allclose(x1, x4))
+        values = [to_numpy(x) for x in values]
+        for x in values:
+            print(x)
+        x0 = values[0]
+        for x in values[1:]:
+            self.assertTrue(np.allclose(x0, x, atol=1e-5))
 
-    def check_numbers_equal(self, operation, x1, x2, x3, x4):
+    def check_numbers_equal(self, operation, values):
         print(f'--- {operation} ---')
-        print(x1, x1.__class__)
-        print(x2, x2.__class__)
-        print(x3, x3.__class__)
-        print(x4, x4.__class__)
-        self.assertTrue(x1 == x2)
-        self.assertTrue(x1 == x3)
-        self.assertTrue(x1 == x4)
+        for x in values:
+            print(x, x.__class__)
+        x0 = values[0]
+        for x in values[1:]:
+            self.assertAlmostEqual(x0, x, delta=1e-5)
 
     def make_variables(self):
         m = 2
@@ -84,7 +77,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.zeros(m, n)
         x3 = tf_.zeros(m, n)
         x4 = torch_.zeros(m, n)
-        self.check_arrays_equal('zeros', x1, x2, x3, x4)
+        self.check_arrays_equal('zeros', [x1, x2, x3, x4])
 
     def test_ones(self):
         #  (2) ones(m: int, n: int = 1) -> Matrix:
@@ -94,7 +87,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.ones(m, n)
         x3 = tf_.ones(m, n)
         x4 = torch_.ones(m, n)
-        self.check_arrays_equal('ones', x1, x2, x3, x4)
+        self.check_arrays_equal('ones', [x1, x2, x3, x4])
 
     def test_identity(self):
         #  (3) identity(n: int) -> Matrix:
@@ -104,7 +97,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.identity(n)
         x3 = tf_.identity(n)
         x4 = torch_.identity(n)
-        self.check_arrays_equal('identity', x1, x2, x3, x4)
+        self.check_arrays_equal('identity', [x1, x2, x3, x4])
 
     def test_product(self):
         #  (4) product(X: Matrix, Y: Matrix) -> Matrix:
@@ -115,7 +108,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.product(x, y)
         x3 = tf_.product(to_tensorflow(x), to_tensorflow(y))
         x4 = torch_.product(to_torch(x), to_torch(y))
-        self.check_arrays_equal('product', x1, x2, x3, x4)
+        self.check_arrays_equal('product', [x1, x2, x3, x4])
 
     def test_hadamard(self):
         # (5) hadamard(X: Matrix, Y: Matrix) -> Matrix:
@@ -126,7 +119,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.hadamard(x, y)
         x3 = tf_.hadamard(to_tensorflow(x), to_tensorflow(y))
         x4 = torch_.hadamard(to_torch(x), to_torch(y))
-        self.check_arrays_equal('hadamard', x1, x2, x3, x4)
+        self.check_arrays_equal('hadamard', [x1, x2, x3, x4])
 
     def test_diag(self):
         # (6) diag(X: Matrix) -> Matrix:
@@ -137,7 +130,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.diag(x)
         x3 = tf_.diag(to_tensorflow(x))
         x4 = torch_.diag(to_torch(x))
-        self.check_arrays_equal('diag', x1, x2, x3, x4)
+        self.check_arrays_equal('diag', [x1, x2, x3, x4])
 
     def test_Diag(self):
         # (7) Diag(x: Matrix) -> Matrix:
@@ -148,7 +141,7 @@ class TestMatrixOperations(TestCase):
             x2 = np_.Diag(x)
             x3 = tf_.Diag(to_tensorflow(x))
             x4 = torch_.Diag(to_torch(x))
-            self.check_arrays_equal('Diag', x1, x2, x3, x4)
+            self.check_arrays_equal('Diag', [x1, x2, x3, x4])
 
     def test_elements_sum(self):
         # (8) elements_sum(X: Matrix):
@@ -159,7 +152,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.elements_sum(x)
         x3 = tf_.elements_sum(to_tensorflow(x))
         x4 = torch_.elements_sum(to_torch(x))
-        self.check_numbers_equal('elements_sum', x1, x2, x3, x4)
+        self.check_numbers_equal('elements_sum', [x1, x2, x3, x4])
 
     def test_column_repeat(self):
         # (9) column_repeat(x: Matrix, n: int) -> Matrix:
@@ -170,7 +163,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.column_repeat(x, n)
         x3 = tf_.column_repeat(to_tensorflow(x), n)
         x4 = torch_.column_repeat(to_torch(x), n)
-        self.check_arrays_equal('column_repeat', x1, x2, x3, x4)
+        self.check_arrays_equal('column_repeat', [x1, x2, x3, x4])
 
     def test_row_repeat(self):
         # (10) row_repeat(xr: Matrix, m: int) -> Matrix:
@@ -181,7 +174,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.row_repeat(x, n)
         x3 = tf_.row_repeat(to_tensorflow(x), n)
         x4 = torch_.row_repeat(to_torch(x), n)
-        self.check_arrays_equal('row_repeat', x1, x2, x3, x4)
+        self.check_arrays_equal('row_repeat', [x1, x2, x3, x4])
 
     def test_columns_sum(self):
         # (11) columns_sum(X: Matrix) -> Matrix:
@@ -192,7 +185,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.columns_sum(x)
         x3 = tf_.columns_sum(to_tensorflow(x))
         x4 = torch_.columns_sum(to_torch(x))
-        self.check_arrays_equal('columns_sum', x1, x2, x3, x4)
+        self.check_arrays_equal('columns_sum', [x1, x2, x3, x4])
 
     def test_rows_sum(self):
         # (12) rows_sum(X: Matrix) -> Matrix:
@@ -203,7 +196,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.rows_sum(x)
         x3 = tf_.rows_sum(to_tensorflow(x))
         x4 = torch_.rows_sum(to_torch(x))
-        self.check_arrays_equal('rows_sum', x1, x2, x3, x4)
+        self.check_arrays_equal('rows_sum', [x1, x2, x3, x4])
 
     def test_columns_max(self):
         # (13) columns_max(X: Matrix) -> Matrix:
@@ -214,7 +207,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.columns_max(x)
         x3 = tf_.columns_max(to_tensorflow(x))
         x4 = torch_.columns_max(to_torch(x))
-        self.check_arrays_equal('columns_max', x1, x2, x3, x4)
+        self.check_arrays_equal('columns_max', [x1, x2, x3, x4])
 
     def test_rows_max(self):
         # (14) rows_max(X: Matrix) -> Matrix:
@@ -225,7 +218,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.rows_max(x)
         x3 = tf_.rows_max(to_tensorflow(x))
         x4 = torch_.rows_max(to_torch(x))
-        self.check_arrays_equal('rows_max', x1, x2, x3, x4)
+        self.check_arrays_equal('rows_max', [x1, x2, x3, x4])
 
     def test_columns_mean(self):
         # (15) columns_mean(X: Matrix) -> Matrix:
@@ -236,7 +229,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.columns_mean(x)
         x3 = tf_.columns_mean(to_tensorflow(x))
         x4 = torch_.columns_mean(to_torch(x))
-        self.check_arrays_equal('columns_mean', x1, x2, x3, x4)
+        self.check_arrays_equal('columns_mean', [x1, x2, x3, x4])
 
     def test_rows_mean(self):
         # (16) rows_mean(X: Matrix) -> Matrix:
@@ -247,7 +240,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.rows_mean(x)
         x3 = tf_.rows_mean(to_tensorflow(x))
         x4 = torch_.rows_mean(to_torch(x))
-        self.check_arrays_equal('rows_mean', x1, x2, x3, x4)
+        self.check_arrays_equal('rows_mean', [x1, x2, x3, x4])
 
     def test_apply(self):
         # (17) apply(f, X: Matrix) -> Matrix:
@@ -258,7 +251,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.apply(f, x)
         x3 = tf_.apply(f, to_tensorflow(x))
         x4 = torch_.apply(f, to_torch(x))
-        self.check_arrays_equal('apply', x1, x2, x3, x4)
+        self.check_arrays_equal('apply', [x1, x2, x3, x4])
 
     def test_exp(self):
         # (18) exp(X: Matrix) -> Matrix:
@@ -269,7 +262,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.exp(x)
         x3 = tf_.exp(to_tensorflow(x))
         x4 = torch_.exp(to_torch(x))
-        self.check_arrays_equal('exp', x1, x2, x3, x4)
+        self.check_arrays_equal('exp', [x1, x2, x3, x4])
 
     def test_log(self):
         # (19) log(X: Matrix) -> Matrix:
@@ -280,7 +273,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.log(x)
         x3 = tf_.log(to_tensorflow(x))
         x4 = torch_.log(to_torch(x))
-        self.check_arrays_equal('log', x1, x2, x3, x4)
+        self.check_arrays_equal('log', [x1, x2, x3, x4])
 
     def test_inverse(self):
         # (20) inverse(X: Matrix) -> Matrix:
@@ -291,7 +284,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.inverse(x)
         x3 = tf_.inverse(to_tensorflow(x))
         x4 = torch_.inverse(to_torch(x))
-        self.check_arrays_equal('inverse', x1, x2, x3, x4)
+        self.check_arrays_equal('inverse', [x1, x2, x3, x4])
 
     def test_square(self):
         # (21) square(X: Matrix) -> Matrix:
@@ -302,7 +295,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.square(x)
         x3 = tf_.square(to_tensorflow(x))
         x4 = torch_.square(to_torch(x))
-        self.check_arrays_equal('square', x1, x2, x3, x4)
+        self.check_arrays_equal('square', [x1, x2, x3, x4])
 
     def test_sqrt(self):
         # (22) sqrt(X: Matrix) -> Matrix:
@@ -313,7 +306,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.sqrt(x)
         x3 = tf_.sqrt(to_tensorflow(x))
         x4 = torch_.sqrt(to_torch(x))
-        self.check_arrays_equal('sqrt', x1, x2, x3, x4)
+        self.check_arrays_equal('sqrt', [x1, x2, x3, x4])
 
     def test_power_minus_half(self):
         # (23) power_minus_half(X: Matrix) -> Matrix:
@@ -324,7 +317,7 @@ class TestMatrixOperations(TestCase):
         x2 = np_.power_minus_half(x)
         x3 = tf_.power_minus_half(to_tensorflow(x))
         x4 = torch_.power_minus_half(to_torch(x))
-        self.check_arrays_equal('power_minus_half', x1, x2, x3, x4)
+        self.check_arrays_equal('power_minus_half', [x1, x2, x3, x4])
 
 
 class TestConversions(TestCase):
