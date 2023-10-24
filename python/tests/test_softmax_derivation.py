655e5cd88aafef0e7a2dbf5ad226dcfe92eca308
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-10-24 12:53:25 +0200

    Added a softmax derivation test

--- python/tests/test_softmax_derivation.py
+++ python/tests/test_softmax_derivation.py
@@ -19,6 +19,10 @@ def matrix(name: str, rows: int, columns: int) -> Matrix:
     return Matrix(sp.symarray(name, (rows, columns), real=True))
 
 
+def to_matrix(x):
+    return sp.Matrix([[x]])
+
+
 def pp(name: str, x: sp.Matrix):
     print(f'{name} ({x.shape[0]}x{x.shape[1]})')
     for row in x.tolist():
@@ -31,30 +35,19 @@ def pp(name: str, x: sp.Matrix):
     print()
 
 
-def substitute(expr, substitutions: Union[Tuple[Matrix, Matrix], List[Tuple[Matrix, Matrix]]]):
-    if isinstance(substitutions, tuple):
-        substitutions = [substitutions]
-    for (X, Y) in substitutions:
-        assert X.shape == Y.shape
-        m, n = X.shape
-        sigma = ((X[i, j], Y[i, j]) for i in range(m) for j in range(n))
-        expr = expr.subs(sigma)
-    return expr
-
-
 class TestSoftmaxDerivation(TestCase):
     def test1(self):
         K = 3
         z = matrix('z', 1, K)
         y = softmax_rowwise(z)
-        self.assertEqual(softmax_rowwise(z).jacobian(z), Diag(y) - y.T * y)
+        self.assertTrue(equal_matrices(softmax_rowwise(z).jacobian(z), Diag(y) - y.T * y))
 
     def test2(self):
-        K = 2
+        K = 3
         y = matrix('y', 1, K)
         z = matrix('z', 1, K)
-        L1 = lambda Y: sp.Matrix([[elements_sum(Y)]])
-        L2 = lambda Y: sp.Matrix([[elements_sum(hadamard(Y, Y))]])
+        L1 = lambda Y: to_matrix(elements_sum(Y))
+        L2 = lambda Y: to_matrix(elements_sum(hadamard(Y, Y)))
 
         for L in [L1, L2]:
             y_z = softmax_rowwise(z)
@@ -62,6 +55,25 @@ class TestSoftmaxDerivation(TestCase):
             dL_dy = substitute(L(y).jacobian(y), (y, y_z))
             dL_dz = L(y_z).jacobian(z)
             self.assertTrue(equal_matrices(dL_dz, dL_dy * dsoftmax_z_dz))
+
+    def test3(self):
+        K = 3
+        y = matrix('y', 1, K)
+        z = matrix('z', 1, K)
+        L1 = lambda Y: to_matrix(elements_sum(Y))
+        L2 = lambda Y: to_matrix(elements_sum(hadamard(Y, Y)))
+
+        for L in [L1, L2]:
+            y_z = softmax_rowwise(z)
+            dsoftmax_z_dz = y_z.jacobian(z)
+            dL_dy = substitute(L(y).jacobian(y), (y, y_z))
+            dL_dz = L(y_z).jacobian(z)
+            Dy = dL_dy
+            Dz = dL_dz
+            self.assertTrue(equal_matrices(Dz, Dy * dsoftmax_z_dz))
+            self.assertTrue(equal_matrices(dsoftmax_z_dz, Diag(y_z) - y_z.T * y_z))
+            self.assertTrue(equal_matrices(Dz, Dy * (Diag(y_z) - y_z.T * y_z)))
+            self.assertTrue(equal_matrices(Dz, hadamard(y_z, Dy) - Dy * y_z.T * y_z))
 
 
 if __name__ == '__main__':
