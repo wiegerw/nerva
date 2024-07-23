a84f0b92f43cf1454e4c4ca8ff99e7efa3af211d
“wieger” <j.w.wesselink@tue.nl>
2024-07-23 13:16:33 +0200

    Fixed layer_test

--- tests/make_multilayer_perceptron_tests.py
+++ tests/make_multilayer_perceptron_tests.py
@@ -31,8 +31,10 @@ class MLP(nn.Module):
         return x
 
 
-def train(out, M, X, T, optimizer, loss_fn, epochs, rowwise):
+def train(M, X, T, optimizer, loss_fn, epochs):
     N, _ = X.shape  # N is the number of training examples
+
+    result = []
 
     for epoch in range(epochs):
         optimizer.zero_grad()
@@ -41,9 +43,11 @@ def train(out, M, X, T, optimizer, loss_fn, epochs, rowwise):
         loss = loss_fn(Y, T)
         loss.backward()
         DY = Y.grad.detach()
-        print_cpp_matrix_declaration(out, f'Y{epoch+1}', Y.detach().numpy() if rowwise else Y.detach().numpy().T)
-        print_cpp_matrix_declaration(out, f'DY{epoch+1}', DY.detach().numpy() if rowwise else DY.detach().numpy().T)
+        result.append(Y.detach().numpy())
+        result.append(DY.detach().numpy())
         optimizer.step()
+
+    return result
 
 
 def make_testcase(out: StringIO, name: str, sizes: list[int], N: int, rowwise=False):
@@ -54,9 +58,6 @@ def make_testcase(out: StringIO, name: str, sizes: list[int], N: int, rowwise=Fa
     :param N: The number of samples in the data set.
     """
 
-    out.write(f'TEST_CASE("{name}")\n')
-    out.write('{\n')
-
     D = sizes[0]   # the input size
     K = sizes[-1]  # the output size
     lr = 0.01      # the learning rate
@@ -64,31 +65,57 @@ def make_testcase(out: StringIO, name: str, sizes: list[int], N: int, rowwise=Fa
     X = random_float_matrix(N, D, 0.0, 1.0).astype(np.float32)
     T = make_target_rowwise(N, K).astype(np.float32)  # a one hot encoded target
 
-    print_cpp_matrix_declaration(out, 'X', X, rowwise=rowwise)
-    print_cpp_matrix_declaration(out, 'T', T, rowwise=rowwise)
-
     X = torch.from_numpy(X)
     T = torch.from_numpy(T)
 
     device = torch.device("cpu")
     M = MLP(sizes).to(device)
 
-    for i, layer in enumerate(M.layers):
-        print_cpp_matrix_declaration(out, f'W{i+1}', layer.weight.detach().numpy(), rowwise=rowwise)
-        print_cpp_matrix_declaration(out, f'b{i+1}', layer.bias.detach().numpy(), rowwise=rowwise)
-
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.SGD(M.parameters(), lr=lr)
     epochs = 2
 
-    train(out, M, X, T, optimizer, loss_fn, epochs, rowwise)
+    Y1, DY1, Y2, DY2 = train(M, X, T, optimizer, loss_fn, epochs)
+    X = X.detach().numpy()
+    T = T.detach().numpy()
+    W1 = M.layers[0].weight.detach().numpy()
+    b1 = M.layers[0].bias.detach().numpy()
+    W2 = M.layers[1].weight.detach().numpy()
+    b2 = M.layers[1].bias.detach().numpy()
+    W3 = M.layers[2].weight.detach().numpy()
+    b3 = M.layers[2].bias.detach().numpy()
 
+    if not rowwise:
+        X = X.T
+        T = T.T
+        Y1 = Y1.T
+        DY1 = DY1.T
+        Y2 = Y2.T
+        DY2 = DY2.T
+
+    out.write(f'TEST_CASE("{name}")\n')
+    out.write('{\n')
+    print_cpp_matrix_declaration(out, 'X', X)
+    print_cpp_matrix_declaration(out, 'T', T)
+    print_cpp_matrix_declaration(out, 'W1', W1)
+    print_cpp_matrix_declaration(out, 'b1', b1, rowwise=rowwise)
+    print_cpp_matrix_declaration(out, 'W2', W2)
+    print_cpp_matrix_declaration(out, 'b2', b2, rowwise=rowwise)
+    print_cpp_matrix_declaration(out, 'W3', W3)
+    print_cpp_matrix_declaration(out, 'b3', b3, rowwise=rowwise)
+    print_cpp_matrix_declaration(out, 'Y1', Y1)
+    print_cpp_matrix_declaration(out, 'DY1', DY1)
+    print_cpp_matrix_declaration(out, 'Y2', Y2)
+    print_cpp_matrix_declaration(out, 'DY2', DY2)
+    sizes_text = ', '.join(str(x) for x in sizes)
     out.write(f'  scalar lr = {lr};\n')
-    out.write('  test_mlp_execution(X, T, W1, b1, W2, b2, W3, b3, Y1, DY1, Y2, DY2, lr);\n')
+    out.write(f'  std::vector<long> sizes = {{{sizes_text}}};\n')
+    out.write(f'  long N = {N};\n')
+    out.write('  test_mlp_execution(X, T, W1, b1, W2, b2, W3, b3, Y1, DY1, Y2, DY2, lr, sizes, N);\n')
     out.write('}\n\n')
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description='Convert and publish analysis files')
     parser.add_argument('--colwise', help='Generate tests for colwise layout', action='store_true')
     args = parser.parse_args()
@@ -103,5 +130,10 @@ if __name__ == '__main__':
     out = StringIO()
     for i in range(3):
         make_testcase(out, f"test_mlp{i+1}", sizes=layer_sizes[i], N=example_counts[i], rowwise=rowwise)
+        break
     text = out.getvalue()
     insert_text_in_file('multilayer_perceptron_test.cpp', text)
+
+
+if __name__ == '__main__':
+    main()
